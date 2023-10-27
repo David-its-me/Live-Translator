9e87c075dfbd45e219887bd801457dd2497b6188
David-its-me <45200216+David-its-me@users.noreply.github.com>
2023-10-27 23:59:39 +0200

    first attempt to run m4t in docker with uvicorn api

--- TranslationService/src/main.py
+++ TranslationService/src/main.py
@@ -1,15 +1,44 @@
-from typing import Union
+from typing import Union, Annotated
 
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import HTMLResponse
+import translator
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/", response_class=HTMLResponse)
+def home():
+    html_content = """
+        <body>
+        <form action="/files/" enctype="multipart/form-data" method="post">
+            <input name="files" type="file" multiple />
+            <input type="submit" />
+        </form>
+        <form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+            <input name="files" type="file" multiple />
+            <input type="submit" />
+        </form>
+        </body>"""
+    
+    return HTMLResponse(content=html_content, status_code=200)
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/files/")
+async def create_file(file: Annotated[bytes, File()]):
+    return {"file_size": len(file)}
+
+
+@app.post("/uploadfiles/")
+async def create_upload_files(files: list[UploadFile]):
+    for file in files:
+        if file.content_type == "audio/wav":
+            content = await file.read()
+            translator.predict_S2ST(audio_input=content, target_language='English')
+        else:
+            raise HTTPException(status_code=415, detail="A audio/wav file is required, but a {} file was uploaded".format(file.content_type))
+    return {"filename": file.filename}
--- TranslationService/src/translator.py
+++ TranslationService/src/translator.py
@@ -1,21 +1,61 @@
 import torch
 import torchaudio
+import languages
+import numpy as np
 from seamless_communication.models.inference import Translator
+from io import BytesIO
 
-"""
+TASK_NAMES = {
+    "S2ST": "(Speech to Speech translation)",
+    "S2TT": "(Speech to Text translation)",
+    "T2ST": "(Text to Speech translation)",
+    "T2TT": "(Text to Text translation)",
+    "ASR": "(Automatic Speech Recognition)",
+}
+
+AUDIO_SAMPLE_RATE = 16000.0
+MAX_INPUT_AUDIO_LENGTH = 60  # in seconds
+DEFAULT_TARGET_LANGUAGE = "English"
+
 # Initialize a Translator object with a multitask model, vocoder on the GPU.
-translator = Translator("seamlessM4T_large", "vocoder_36langs", torch.device("cuda:0"), torch.float16)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+translator = Translator(
+    model_name_or_card="seamlessM4T_large",
+    vocoder_name_or_card="vocoder_36langs", 
+    device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),
+    dtype=torch.float16 if "cuda" in device.type else torch.float32,)
 
+def predict_S2ST(
+        audio_input: bytes,
+        source_language: str | None,
+        target_language: str,) -> tuple[tuple[int, np.ndarray], str]:
+    
+    task_name = task_name.split()[0]
+    source_language_code = languages.LANGUAGE_NAME_TO_CODE[source_language] if source_language else None
+    target_language_code = languages.LANGUAGE_NAME_TO_CODE[target_language]
 
-# S2ST
-translated_text, wav, sr = translator.predict(<path_to_input_audio>, "s2st", <tgt_lang>)
+    # change result bytes stream to file-like object
+    input_data = BytesIO(audio_input)
 
-wav, sr = translator.synthesize_speech(<speech_units>, <tgt_lang>)
-
-# Save the translated audio generation.
-torchaudio.save(
-    <path_to_save_audio>,
-    wav[0].cpu(),
-    sample_rate=sr,
-)
-"""
+    input_array, orig_freqency = torchaudio.load(input_data)
+    new_input_array = torchaudio.functional.resample(
+        input_array, 
+        orig_freq=orig_freqency, 
+        new_freq=AUDIO_SAMPLE_RATE
+    )
+    max_length = int(MAX_INPUT_AUDIO_LENGTH * AUDIO_SAMPLE_RATE)
+    if new_input_array.shape[1] > max_length:
+        new_input_array = new_input_array[:, :max_length]
+        print(
+            f"Input audio is too long. Only the first {MAX_INPUT_AUDIO_LENGTH} seconds is used."
+        )
+    torchaudio.save("buffer_audio.wav", new_input_array, sample_rate=int(AUDIO_SAMPLE_RATE))
+    
+    text_out, wav_result, sample_rate = translator.predict(
+        input=input_data,
+        task_str="S2ST",
+        tgt_lang=target_language_code,
+        src_lang=source_language_code,
+        ngram_filtering=True,
+    )
+    return (sample_rate, wav_result.cpu().detach().numpy()), text_out
